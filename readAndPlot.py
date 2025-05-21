4ad11efbd2fdbc914c3f6e6f5bc502f57eff9eb1
raynah fandozzi <f.raynah@wustl.edu>
2025-05-20 21:41:27 -0400

    .spa conv to .txt and plots

--- readAndPlot.py
+++ readAndPlot.py
@@ -1,7 +1,12 @@
 import os
 import pandas as pd
+
+import matplotlib
+matplotlib.use('Agg')  # for SSH or headless environments
 import matplotlib.pyplot as plt
+
 import subprocess
+
 
 #configuring where the directories are
 spa_path='./spa2txt/build/spa2txt'
@@ -27,5 +32,29 @@ for root,dirs,files in os.walk(main_folder):
 
 
             #creating the subfolders in the directories
-            os.makedirs(output_folder, exist_ok=True)
-            os.makedirs(plot_folder, exist_ok=True)
+            os.makedirs(txt_output_folder, exist_ok=True)
+            os.makedirs(plot_output_folder, exist_ok=True)
+
+            target_txt_path = os.path.join(txt_output_folder, file + '.txt')
+            os.rename(raw_txt_path, target_txt_path)
+            
+            with open(target_txt_path, "r") as f:
+                lines = f.readlines()[1:]  #skipping header
+            wavenumber = []
+            absorbance = []
+            
+            for line in lines:
+                data=line.strip().split()
+                wavenumber.append(float(data[0]))
+                absorbance.append(float(data[1]))
+
+            plt.figure()
+            plt.plot(wavenumber, absorbance)
+            plt.grid(True)
+            plt.xlabel("Wavenumber")
+            plt.ylabel("Absorbance")
+            plt.title(file)
+            plot_path = os.path.join(plot_output_folder, file + '.png')
+            plt.savefig(plot_path)
+            plt.close()
+
